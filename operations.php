0c7694982a863563fdd59db573c1b4677fd13845
prasainpti <110977009+prasainpti@users.noreply.github.com>
2022-12-22 09:33:47 +0545

    Create operations.php

---
+++ operations.php
@@ -0,0 +1,10 @@
+<?php
+$x=10;
+$y=5;
+echo "the sum of $x and $y is ".$x + $y. "<br>";
+
+echo "the subbtraction of $x and $y". $x - $y. "<br";
+
+echo "the multiplication $x and $y is". $x * $y;
+
+?>
