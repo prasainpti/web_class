664b996204845f1a0e6d98d655f60368fd12ae0b
prasainpti <110977009+prasainpti@users.noreply.github.com>
2022-12-22 09:34:35 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$x="<h1> This is a variable x </h1>";
+echo "hello! world";
+# this is a variable
+$z="this is a variable 1";
+$y="this is a variable 2";
+echo $x;
+echo $z . ".". $y;
+/* this is a practise folder*/
+?>
