d37c23b4096a0c3df1d1c116c82ffcf8dec18c50
prasainpti <110977009+prasainpti@users.noreply.github.com>
2022-12-22 09:32:08 +0545

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+$xyz = 'welcome';
+$XYZ = 'welcome';
+$Xyz = 'welcome';
+$xyz1231 = 'welcome';
+$xyZ = 'welcome';
+$XYZ = 'welcome';
+$_XYZ = 'welcome';
+$XYZ_xyz = 'welcome';
+echo "$xyz";
+?>
